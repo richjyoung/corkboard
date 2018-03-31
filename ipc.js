2b49757d3a3211c75089d2a10b0067589d820cc8
Rich Young <rich@richjyoung.co.uk>
2018-03-31 17:22:17 +0100

    Added window close handler and cleaned debug

--- ipc.js
+++ ipc.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 module.exports = function(win) {
 
     ipcMain.on('toggle_fullscreen', function(event, arg) {
-        console.log('toggle_fullscreen');
+        console.log('Event received: toggle_fullscreen');
         win.setFullScreen(!win.isFullScreen());
     });
 
     ipcMain.on('toggle_devtools', function(event, arg) {
-        console.log('toggle_devtools');
+        console.log('Event received: toggle_devtools');
         win.toggleDevTools();
     });
 
--- main.js
+++ main.js
@@ -8,5 +8,18 @@ let win;
 
 app.on('ready', function() {
     win = window('index.html');
+    win.on('closed', function() {
+        console.log('Window closed');
+        win = null;
+    });
     ipc_setup(win);
+    console.log('Application started');
+});
+
+app.on('window-all-closed', function() {
+    console.log('Application windows closed');
+    if(process.platform !== 'darwin') {
+        console.log('Application exited');    
+        app.quit();
+    }
 });
