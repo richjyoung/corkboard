8f43235e8b9a0ae0212f1a859808f85d6f885435
Rich Young <rich@richjyoung.co.uk>
2018-04-10 21:24:08 +0100

    Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
+    env: {
+        browser: true,
+        es6: true,
+        node: true
     },
     "extends": ["eslint:recommended", "plugin:vue/recommended"],
-    "parserOptions": {
-        "sourceType": "module"
+    parserOptions: {
+        sourceType: 'module'
     },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": [
-            "error"
-        ],
-        "no-trailing-spaces": [
-            "error"
-        ],
-        "object-curly-spacing": [
-            "error",
-            "always"
-        ],
-        "curly": [
-            "error",
-            "all"
-        ],
-        "key-spacing": [
-            "error"
-        ],
-        "vue/require-default-prop": [
-            "off"
-        ],
-        "vue/html-indent": [
-            "error",
-            4
-        ],
-        "vue/html-closing-bracket-newline": [
-            "error"
-        ],
-        "vue/html-closing-bracket-spacing": [
-            "error"
-        ],
-        "vue/prop-name-casing": [
-            "error"
-        ]
+    plugins: ['vue', 'prettier'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'windows'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'no-console': ['error'],
+        'no-trailing-spaces': ['error'],
+        'object-curly-spacing': ['error', 'always'],
+        curly: ['error', 'all'],
+        'key-spacing': ['error'],
+        'vue/require-default-prop': ['off'],
+        'vue/html-indent': ['error', 4],
+        'vue/html-closing-bracket-newline': ['error'],
+        'vue/html-closing-bracket-spacing': ['error'],
+        'vue/prop-name-casing': ['error'],
+        'prettier/prettier': ['error']
     }
 };
